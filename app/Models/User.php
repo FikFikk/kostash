0701846f2c48ec9bc65c054a3c3247154c8007b0
FikFikk <fikri225456@gmail.com>
2025-07-30 20:44:41 +0700

    fix(User): rename tanggal_masuk to date_entry

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'nik',
         'address',
         'alamat',
-        'tanggal_masuk',
+        'date_entry',
         'room_id',
         'role',
         'provider',
@@ -61,6 +61,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'date_entry' => 'date',
             'password' => 'hashed',
         ];
     }

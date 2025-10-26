19f98ca198a034b02757f7cf62073d6b6018b6d9
FikFikk <fikri225456@gmail.com>
2025-10-26 13:36:26 +0700

    Fixing only caraosal and gallery active in landing page

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -12,9 +12,13 @@ class Gallery extends Model
         'uploader_name',
         'categories',
         'description',
+        'is_gallery',
+        'is_carousel',
     ];
 
     protected $casts = [
         'categories' => 'array',
+        'is_gallery' => 'boolean',
+        'is_carousel' => 'boolean',
     ];
 }
